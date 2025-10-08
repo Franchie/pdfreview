8b3381b1b27f808ec32f87bac057bd9f7ef8ea7c
Bradley Smith <bradley.smith@arm.com>
2025-10-08 16:38:02 +0100

    Make user migration robust to real data

--- scripts/fetch_users.py
+++ scripts/fetch_users.py
@@ -16,31 +16,38 @@ def iter_graph_collection(access_token: str, initial_url: str) -> Generator[dict
     while url:
         print(".", end="", flush=True)
         data: dict[str, Any] = session.request("GET", url, headers=headers).json()
+        if "error" in data:
+            raise requests.RequestException(data["error"])
         yield from data.get("value", [])
         url = data.get("@odata.nextLink")
     print("", flush=True)
 
 
 def main():
-    url = "https://graph.microsoft.com/v1.0/users?$select=id,displayName,mail&$top=999"
+    url = "https://graph.microsoft.com/v1.0/users?$select=id,displayName,mail,proxyAddresses&$top=999"
 
-    rows: list[dict[str, str]] = list(iter_graph_collection(os.environ["MIGRATION_ACCESS_TOKEN"], url))
+    rows: list[dict[str, Any]] = list(iter_graph_collection(os.environ["MIGRATION_ACCESS_TOKEN"], url))
 
     email_map = {}
     name_map = {}
 
-    for row in rows:
-        if not row["mail"] or not row["mail"].endswith("@arm.com"):
+    for row in [x for x in rows if x["mail"]]:
+        current_email = row["mail"].lower()
+        other_email = [x.split(":")[-1].lower() for x in row["proxyAddresses"]]
+
+        if not current_email.endswith("@arm.com"):
             continue
 
-        if row["mail"].lower() in email_map:
-            raise ValueError(f"Duplicate email: {row["mail"].lower()}")
+        for email in set([current_email] + [x for x in other_email if x.endswith("@arm.com")]):
+            if email in email_map:
+                raise ValueError(f"Duplicate email: {email}")
+
+            email_map[email] = {"uid": row["id"], "name": row["displayName"], "email": email}
+
         if row["displayName"] in name_map:
             name_map[row["displayName"]] = None
-            continue
-
-        email_map[row["mail"].lower()] = {"uid": row["id"], "name": row["displayName"]}
-        name_map[row["displayName"]] = {"uid": row["id"], "email": row["mail"].lower()}
+        else:
+            name_map[row["displayName"]] = email_map[current_email]
 
     with open(f"{os.path.dirname(os.path.realpath(__file__))}/../alembic/user_map.json", "w", encoding="utf-8") as f:
         json.dump({"email_map": email_map, "name_map": name_map}, f, indent=4)
